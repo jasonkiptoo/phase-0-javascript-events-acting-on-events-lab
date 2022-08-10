a40ad54f130ba4d7c2da74504db21e0439e8e50e
jasonkiptoo <kiptoojason@gmail.com>
2022-08-10 12:27:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+
+
+
+
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerLeft();
+    }
+  });
+
+
+
+
+    function moveDodgerLeft(){
+
+    
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+  
+      dodger.style.left = `${left - 1}px`;
+
+
+    }
+
+    function moveDodgerRight(){
+
+    
+        const leftNumbers = dodger.style.left.replace("px", "");
+        const left = parseInt(leftNumbers, 10);
+    
+        dodger.style.left = `${left + 1}px`;
+  
+  
+      }
